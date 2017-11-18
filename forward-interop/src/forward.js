8cbbb219e8c0932644cd2a21cbfac85905107551
Bradley Bridges <blbridges96@gmail.com>
2017-11-18 13:13:35 -0600

    Uploading telemetry at 5Hz to the interop server
    Note that currently the service is getting JSON data from the telemetry service
    instead of protobuf for now.
    
    Also removed colored output since Node.js wasn't cooperating when there's no
    tty.

--- forward-interop/src/forward.js
+++ forward-interop/src/forward.js
@@ -1,18 +1,51 @@
-import 'colors';
+import async from 'async';
+import request from 'request-promise-native';
 
 import AUVSIClient from 'auvsisuas-client';
-import { InteropTelem } from './messages/telemetry_pb.js'
+import { InteropTelem } from './messages/telemetry_pb.js';
 
-let url = process.env.INTEROP_URL;
+let interopUrl = process.env.INTEROP_URL;
 let username = process.env.USERNAME;
 let password = process.env.PASSWORD;
 
+let telemUrl = process.env.TELEMETRY_URL;
+
 let client = new AUVSIClient();
 
-client.login('http://' + url, username, password, 5000)
+/**
+ * Get the latest telemetry and send it to the interop server.
+ *
+ * @return {Promise.<void, Error>}
+ */
+function sendTelem() {
+    return request.get('http://' + telemUrl + '/api/interop-telem?json=true')
+        .then(JSON.parse)
+        .then((telem) => {
+            return client.postTelemetry({
+                lat: telem.lat,
+                lon: telem.lon,
+                alt_msl: telem.altFeetMsl,
+                yaw: telem.yaw
+            });
+        });
+}
+
+client.login('http://' + interopUrl, username, password, 5000)
+    .then(() => console.log('Login Sucessful.'))
+    .then(() => console.log('Now forwarding telemetry...'))
     .then(() => {
-        console.log('Login Sucessful.'.green);
+        // This promise should never actually resolve, if so, that
+        // means we're not continuously sending telemetry.
+        return new Promise((resolve, reject) => {
+            // Continuously send telemetry every 200 ms.
+            async.forever((next) => {
+                sendTelem()
+                    .then(() => setTimeout(next, 200))
+                    .catch(next);
+            }, reject);
+        });
     })
+    .then(() => { throw new Error('Telemetry loop stopped!') })
     .catch((err) => {
         console.log(err);
         process.exit(1);
