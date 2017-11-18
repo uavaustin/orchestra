4720aac98d7b313337dc146a7969d601fa6d03ed
Bradley Bridges <blbridges96@gmail.com>
2017-11-18 01:39:00 -0600

    Added more checks and timestamps to `wait-for-it.sh`, waiting for telem service
    `wait-for-it.sh` will poll the server every second and display a time every 5
    seconds. 10 seconds will print a yellow warning and 30 and above will print
    red.
    
    The forward-interop service also now waits for the telemetry service.

--- common/scripts/wait-for-it.sh
+++ common/scripts/wait-for-it.sh
@@ -18,6 +18,20 @@ send() {
     curl --output /dev/null --silent --head $url
 }
 
+wait_status() {
+    printf "Waiting for response from $name... "
+
+    if [ "$1" -lt 10 ]; then
+        printf "(~"$1"s)"
+    elif [ "$1" -lt 30 ]; then
+        printf "\033[33m(~"$1"s)\033[0m"
+    else
+        printf "\033[31m(~"$1"s)\033[0m"
+    fi
+
+    printf "\n"
+}
+
 send
 
 if [ "$?" -ne 0 ]; then
@@ -25,10 +39,17 @@ if [ "$?" -ne 0 ]; then
     printf "Polling $name until it is online.\n"
     printf "\033[0m"
 
+    seconds=0
+
     # Block until we get something
     until $(send); do
-        sleep 5
-        printf "Waiting for response from $name...\n"
+        seconds=$((seconds + 1))
+
+        sleep 1
+
+        if [ $((seconds % 5)) -eq 0 ]; then
+            wait_status $seconds
+        fi
     done
 fi
 
