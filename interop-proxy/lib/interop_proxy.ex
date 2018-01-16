0d57f0a92d7bdebcb033ef77469c9e6ea8b181cc
Bradley Bridges <blbridges96@gmail.com>
2018-01-16 13:21:24 -0600

    Adding functions in InteropProxy module that don't raise exceptions

--- interop-proxy/lib/interop_proxy.ex
+++ interop-proxy/lib/interop_proxy.ex
@@ -16,33 +16,55 @@ defmodule InteropProxy do
   @doc """
   Return the active mission on the server.
   """
-  def get_active_mission! do
-    {:ok, missions} = Request.get_missions url(), cookie()
+  def get_active_mission do
+    case Request.get_missions url(), cookie() do
+      {:ok, missions} ->
+        message = missions
+        |> Enum.find(fn mission -> mission["active"] === true end)
+        |> Sanitize.sanitize_mission
 
-    missions
-    |> Enum.find(fn mission -> mission["active"] === true end)
-    |> Sanitize.sanitize_mission
+        {:ok, message}
+      {:error, _} = other ->
+        other
+    end
   end
+
+  @doc """
+  Same as `get_active_mission/0`, but raises an exception on failure.
+  """
+  def get_active_mission!, do: when_ok get_active_mission()
 
   @doc """
   Return the stationary and moving obstacles on the server.
   """
-  def get_obstacles! do
-    {:ok, obstacles} = Request.get_obstacles url(), cookie()
-
-    Sanitize.sanitize_obstacles obstacles
+  def get_obstacles do
+    case Request.get_obstacles url(), cookie() do
+      {:ok, obstacles}    -> {:ok, Sanitize.sanitize_obstacles(obstacles)}
+      {:error, _} = other -> other
+    end
   end
+
+  @doc """
+  Same as `get_obstacles/0`, but raises an exception on failure.
+  """
+  def get_obstacles!, do: when_ok get_obstacles()
 
   @doc """
   Post telemetry to the server.
   """
-  def post_telemetry!(%InteropTelem{} = telem) do
+  def post_telemetry(%InteropTelem{} = telem) do
     outgoing_telem = Sanitize.sanitize_outgoing_telemetry telem
 
-    {:ok, message} = Request.post_telemetry url(), cookie(), outgoing_telem
-
-    Sanitize.sanitize_message message
+    case Request.post_telemetry url(), cookie(), outgoing_telem do
+      {:ok, message}      -> {:ok, Sanitize.sanitize_message(message)}
+      {:error, _} = other -> other
+    end
   end
+
+  @doc """
+  Same as `post_telemetry/1`, but raises an exception on failure.
+  """
+  def post_telemetry!(telem), do: when_ok post_telemetry(telem)
 
   @doc """
   Post a new odlc on the server.
@@ -50,20 +72,30 @@ defmodule InteropProxy do
   Note that images can be included with the odlc and will be send in
   an another request.
   """
-  def post_odlc!(%Odlc{} = odlc) do
+  def post_odlc(%Odlc{} = odlc) do
     {outgoing_odlc, image} = Sanitize.sanitize_outgoing_odlc odlc
 
-    {:ok, returned} = Request.post_odlc url(), cookie(), outgoing_odlc
+    case Request.post_odlc url(), cookie(), outgoing_odlc do
+      {:ok, returned} ->
+        returned = Sanitize.sanitize_odlc returned
 
-    returned = Sanitize.sanitize_odlc returned
-
-    if image !== <<>> do
-      {:ok, _} = Request.post_odlc_image url(), cookie(), returned.id, image
-      returned
-    else
-      returned
+        if image !== <<>> do
+          case Request.post_odlc_image url(), cookie(), returned.id, image do
+            {:ok, _}            -> {:ok, returned}
+            {:error, _} = other -> other
+          end
+        else
+          {:ok, returned}
+        end
+      {:error, _} = other ->
+        other
     end
   end
+
+  @doc """
+  Same as `post_odlc/1`, but raises an exception on failure.
+  """
+  def post_odlc!(odlc), do: when_ok post_odlc(odlc)
 
   @doc """
   Get all odlcs on the server.
@@ -71,23 +103,41 @@ defmodule InteropProxy do
   By default images are not also returned for each target, to return
   the images, pass in `image: true` with the options.
   """
-  def get_odlcs!(opts \\ []) do
-    {:ok, odlcs} = Request.get_odlcs url(), cookie()
+  def get_odlcs(opts \\ []) do
+    case Request.get_odlcs url(), cookie() do
+      {:ok, odlcs} ->
+        image_resp_list = Enum.map odlcs, fn odlc ->
+          case Keyword.get opts, :image, false do
+            true  -> Request.get_odlc_image url(), cookie(), odlc["id"]
+            false -> {:ok, <<>>}
+          end
+        end
 
-    odlcs
-    |> Enum.map(fn odlc ->
-      image = case Keyword.get opts, :image, false do
-        true ->
-          {:ok, image} = Request.get_odlc_image url(), cookie(), odlc["id"]
-          image
-        false ->
-          <<>>
-      end
+        first_error = Enum.find image_resp_list, fn
+          {:ok, _}    -> false
+          {:error, _} -> true
+        end
 
-      Sanitize.sanitize_odlc odlc, image
-    end)
-    |> Sanitize.sanitize_odlc_list
+        if first_error === nil do
+          message = Enum.zip(odlcs, image_resp_list)
+          |> Enum.map(fn {odlc, {:ok, image}} ->
+            Sanitize.sanitize_odlc odlc, image
+          end)
+          |> Sanitize.sanitize_odlc_list
+
+          {:ok, message}
+        else
+          first_error
+        end
+      {:error, _} = other ->
+        other
+    end
   end
+
+  @doc """
+  Same as `get_odlcs/1`, but raises an exception on failure.
+  """
+  def get_odlcs!(opts \\ []), do: when_ok get_odlcs(opts)
 
   @doc """
   Get an odlc on the server by its id.
@@ -95,19 +145,27 @@ defmodule InteropProxy do
   By default images are not also returned for the target, to return
   the image, pass in `image: true` with the options.
   """
-  def get_odlc!(id, opts \\ []) do
-    {:ok, odlc} = Request.get_odlc url(), cookie(), id
+  def get_odlc(id, opts \\ []) do
+     case Request.get_odlc url(), cookie(), id do
+      {:ok, odlc} ->
+        image_resp = case Keyword.get opts, :image, false do
+          true  -> Request.get_odlc_image url(), cookie(), id
+          false -> {:ok, <<>>}
+        end
 
-    image = case Keyword.get opts, :image, false do
-      true ->
-        {:ok, image} = Request.get_odlc_image url(), cookie(), id
-        image
-      false ->
-        <<>>
+        case image_resp do
+          {:ok, image}        -> {:ok, Sanitize.sanitize_odlc(odlc, image)}
+          {:error, _} = other -> other
+        end
+      {:error, _} = other ->
+        other
     end
-
-    Sanitize.sanitize_odlc odlc, image
   end
+
+  @doc """
+  Same as `get_odlc/2`, but raises an exception on failure.
+  """
+  def get_odlc!(id, odlc \\ []), do: when_ok get_odlc(id, odlc)
 
   @doc """
   Update an odlc on the server.
@@ -119,27 +177,46 @@ defmodule InteropProxy do
   an another request, an empty binary value will not replace the
   image on the servers.
   """
-  def put_odlc!(id, %Odlc{} = odlc) do
+  def put_odlc(id, %Odlc{} = odlc) do
     {outgoing_odlc, image} = Sanitize.sanitize_outgoing_odlc odlc
 
-    {:ok, returned} = Request.put_odlc url(), cookie(), id, outgoing_odlc
+    case Request.put_odlc url(), cookie(), id, outgoing_odlc do
+      {:ok, returned} ->
+        returned = Sanitize.sanitize_odlc returned
 
-    returned = Sanitize.sanitize_odlc returned
-
-    if image !== <<>> do
-      {:ok, _} = Request.put_odlc_image url(), cookie(), returned.id, image
-      returned
-    else
-      returned
+        if image !== <<>> do
+          case Request.put_odlc_image url(), cookie(), returned.id, image do
+            {:ok, _} -> {:ok, returned}
+            other    -> other
+          end
+        else
+          {:ok, returned}
+        end
+      {:error, _} = other ->
+        other
     end
   end
 
   @doc """
+  Same as `put_odlc/2`, but raises an exception on failure.
+  """
+  def put_odlc!(id, odlc), do: when_ok put_odlc(id, odlc)
+
+  @doc """
   Delete an odlc on the server.
   """
-  def delete_odlc!(id) do
-    {:ok, message} = Request.delete_odlc url(), cookie(), id
-
-    Sanitize.sanitize_message message
+  def delete_odlc(id) do
+    case Request.delete_odlc url(), cookie(), id do
+      {:ok, message}      -> {:ok, Sanitize.sanitize_message(message)}
+      {:error, _} = other -> other
+    end
   end
+
+  @doc """
+  Same as `delete_odlc/1`, but raises an exception on failure.
+  """
+  def delete_odlc!(id), do: when_ok delete_odlc(id)
+
+  # For the methods which raise errors.
+  defp when_ok({:ok, message}), do: message
 end
