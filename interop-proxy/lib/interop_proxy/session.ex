09ae3cc1ca169386c218583cece1c7cb713899b3
Bradley Bridges <blbridges96@gmail.com>
2018-01-20 22:52:48 -0600

    Adding a watchdog in the session to re-login if the cookie isn't valid
    This runs on an interval of about 5 seconds. This should handle problems
    for when the interop server has to be restarted.

--- interop-proxy/lib/interop_proxy/session.ex
+++ interop-proxy/lib/interop_proxy/session.ex
@@ -1,6 +1,10 @@
 defmodule InteropProxy.Session do
   @moduledoc """
   Keeps an internal state with the url and cookie for requests.
+
+  Will also keep making sure the login is valid every 5 seconds. If
+  the cookie no longer works, because for example, a new server was
+  created, the cookie would be updated.
   """
 
   use Agent
@@ -26,7 +30,13 @@ defmodule InteropProxy.Session do
 
     if resp !== nil, do: send resp, :ok
 
-    Agent.start_link fn -> {url, cookie} end, other_opts
+    {:ok, pid} = Agent.start_link fn -> %{
+      url: url, cookie: cookie, username: username, password: password
+    } end, other_opts
+
+    spawn_link fn -> monitor_cookie pid, url, username, password end
+
+    {:ok, pid}
   end
 
   # Log in, if we couldn't log in, just keep trying.
@@ -42,17 +52,33 @@ defmodule InteropProxy.Session do
     end
   end
 
+  # Keep checking if the cookie is still valid, if it's not, make a
+  # new one for the session.
+  defp monitor_cookie(session, url, username, password) do
+    Process.sleep 5000
+
+    case Request.get_obstacles url, cookie(session) do
+      {:error, :forbidden} ->
+        {^url, cookie} = do_login url, username, password
+        Agent.update session, &Map.put(&1, :cookie, cookie)
+      _ ->
+        nil
+    end
+
+    monitor_cookie session, url, username, password
+  end
+
   @doc """
   Get the url used in the login.
   """
   def url(session \\ __MODULE__) do
-    Agent.get session, fn {url, _} -> url end
+    Agent.get session, &Map.get(&1, :url)
   end
 
   @doc """
   Get the cookie from the login.
   """
   def cookie(session \\ __MODULE__) do
-    Agent.get session, fn {_, cookie} -> cookie end
+    Agent.get session, &Map.get(&1, :cookie)
   end
 end
