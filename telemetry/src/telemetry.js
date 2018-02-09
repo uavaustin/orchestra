6b9c9809d07aed93637905ebf64a68a249cf591a
Bradley Bridges <blbridges96@gmail.com>
2018-02-08 21:53:52 -0600

    Starting fresh with a Node.js-based telemetry service

---
+++ telemetry/src/telemetry.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import interop from './messages/interop_pb';
+import telemetry from './messages/telemetry_pb';
+
+let app = express();
+
+app.get('/api/alive', (req, res) => {
+    res.set('content-type', 'text/plain');
+    res.send('Yes, I\'m alive!\n');
+});
+
+app.listen(5000);
+
+console.log('Running server with Express at http://0.0.0.0:5000');
+
+export { app, server };
