5c156835a3448f13059873668b7c4d83497df48a
Bradley Bridges <blbridges96@gmail.com>
2018-02-24 14:07:01 -0600

    Making returning the image or warped image optional
    By default we'll return the original image, and if requested, we'll
    return the warped one as well (and if we have it).
    
    This is just set by query params on the request... will document that in
    the README later.
    
    Did a little refactor to make this work, and made the two image
    endpoints a little cleaner.

--- imagery/src/app.js
+++ imagery/src/app.js
@@ -2,22 +2,40 @@ import express from 'express';
 
 import { Image } from './messages/imagery_pb';
 
-/** Send a protobuf or JSON if requested. */
-function sendMessage(req, res, msg) {
-    let accept = req.get('accept');
-
-    // If the client wants JSON, we'll send them Protobuf-style JSON
-    // instead, otherwise, send the Protobuf.
-    if (accept === undefined || !accept.startsWith('application/json')) {
-        res.set('content-type', 'application/x-protobuf');
-        res.send(Buffer.from(msg.serializeBinary()));
-    } else {
-        res.send(msg.toObject());
-    }
-}
-
 export function createApp(imageStore) {
     let app = express();
+
+    /** Send a protobuf or JSON if requested. */
+    function sendMessage(req, res, msg) {
+        let accept = req.get('accept');
+
+        // If the client wants JSON, we'll send them Protobuf-style
+        // JSON instead, otherwise, send the Protobuf.
+        if (accept === undefined || !accept.startsWith('application/json')) {
+            res.set('content-type', 'application/x-protobuf');
+            res.send(Buffer.from(msg.serializeBinary()));
+        } else {
+            res.send(msg.toObject());
+        }
+    }
+
+    /** Get a protobuf Image message based on id and query. */
+    async function getImageMessage(id, query) {
+        let msg = await imageStore.getMetadata(id);
+
+        // By default, we should return the original image if it is
+        // available, but not the warped unless it is requested.
+        let original = query.original !== '0' && query.original !== 'false';
+        let warped = query.warped === '1' || query.warped === 'true';
+
+        if (original)
+            msg.setImage(await imageStore.getImage(id));
+
+        if (warped)
+            msg.setWarpedImage(await imageStore.getImage(id, true));
+
+        return msg;
+    }
 
     app.get('/api/image/:id', async (req, res) => {
         let id = parseInt(req.params.id);
@@ -28,18 +46,14 @@ export function createApp(imageStore) {
             return;
         }
 
-        let msg = await imageStore.getMetadata(id);
-
-        msg.setImage(await imageStore.getImage(id));
+        let msg = await getImageMessage(id, req.query);
 
         sendMessage(req, res, msg);
     });
 
     app.get('/api/next-image', (req, res) => {
         imageStore.once('image', async (id) => {
-            let msg = await imageStore.getMetadata(id);
-
-            msg.setImage(await imageStore.getImage(id));
+            let msg = await getImageMessage(id, req.query);
 
             sendMessage(req, res, msg);
         });
