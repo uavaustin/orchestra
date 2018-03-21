a76ea9c6a7425688c7bac27e3ea3697d8c916029
Bradley Bridges <blbridges96@gmail.com>
2018-03-20 23:43:00 -0500

    Allowing jpegs to be used with the file backend
    Using a node graphicsmagick wrapper to do the conversion to a png.

--- services/imagery/src/util.js
+++ services/imagery/src/util.js
@@ -1,3 +1,5 @@
+import gm from 'gm';
+
 /** Convert a Buffer to a Uint8Array for protobuf message objects. */
 export function toUint8Array(buffer) {
     return new Uint8Array(buffer.buffer.slice(
@@ -8,4 +10,14 @@ export function toUint8Array(buffer) {
 /** Wait for an amount of time. */
 export async function wait(milliseconds) {
     return await (new Promise(resolve => setTimeout(resolve, milliseconds)));
+}
+
+/** Convert a JPEG buffer to a PNG. */
+export async function convertJpeg(image) {
+    return await (new Promise((resolve, reject) => {
+        gm(image, 'image.jpeg').toBuffer('PNG', (err, buffer) => {
+            if (err) reject(err);
+            else resolve(buffer);
+        });
+    }));
 }
