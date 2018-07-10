68eb396aa364e40b410676f6cccf0eaa869997d0
Bradley Bridges <blbridges96@gmail.com>
2018-07-09 23:22:39 -0500

    Add tests for the router endpoints

---
+++ services/forward-interop/test/router.test.js
@@ -0,0 +1,61 @@
+import getRawBody from 'raw-body';
+import Koa from 'koa';
+import request from 'supertest';
+
+import { stats } from '../src/messages';
+
+import router from '../src/router';
+
+test('get text from GET /api/alive', async () => {
+  let app = new Koa();
+
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+
+  let server = app.listen();
+
+  try {
+    let res = await request(server).get('/api/alive');
+
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('text/plain');
+    expect(res.text).toEqual('Yo dude, I\'m good.\n');
+  } finally {
+    await new Promise(resolve => server.close(() => resolve()));
+  }
+});
+
+test('get the upload rate from GET /api/upload-rate', async () => {
+  let app = new Koa();
+
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+
+  let testRate = stats.InteropUploadRate.create({
+    time: 1, total_1: 2, total_5: 3, fresh_1: 4, fresh_5: 5
+  });
+
+  app.context.uploadMonitor = {
+    getUploadRate: jest.fn().mockReturnValue(testRate)
+  };
+
+  let server = app.listen();
+
+  try {
+    let res = await request(server)
+      .get('/api/upload-rate')
+      .buffer(true)
+      .parse((res, cb) => {
+        getRawBody(res, (err, body) => {
+          if (err) cb(err);
+          else cb(null, stats.InteropUploadRate.decode(body));
+        });
+      });
+
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/x-protobuf');
+    expect(res.body).toEqual(testRate);
+  } finally {
+    await new Promise(resolve => server.close(() => resolve()));
+  }
+});
