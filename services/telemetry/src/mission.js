f56d85b9fd6d7a750b0b0972112f55e1d1835f7f
Bradley Bridges <blbridges96@gmail.com>
2018-07-28 23:58:25 -0500

    Reformat newer telemetry files
    Formatting with 2 spaces and adding some dropped semicolons.
    
    Older files, which are not expected to remain after the rewrite with
    async/await have not been reformated.

--- services/telemetry/src/mission.js
+++ services/telemetry/src/mission.js
@@ -29,87 +29,87 @@ const MISSION_TIMEOUT = 15000;
  * @returns {Promise<telemetry.RawMission>}
  */
 export async function receiveMission(mav) {
-    let mission = telemetry.RawMission.create();
-    let itemsRemaining = [];
+  let mission = telemetry.RawMission.create();
+  let itemsRemaining = [];
 
-    let reqListInt;
-    let reqItemInt;
-    let onCount;
-    let onItem;
+  let reqListInt;
+  let reqItemInt;
+  let onCount;
+  let onItem;
 
-    let cleanupObj = cleanupAfter(() => {
-        clearInterval(reqListInt);
-        clearInterval(reqItemInt);
-        mav.removeListener('MISSION_COUNT', onCount);
-        mav.removeListener('MISSION_ITEM', onItem);
-    }, MISSION_TIMEOUT, 'mission receiving took too long');
+  let cleanupObj = cleanupAfter(() => {
+    clearInterval(reqListInt);
+    clearInterval(reqItemInt);
+    mav.removeListener('MISSION_COUNT', onCount);
+    mav.removeListener('MISSION_ITEM', onItem);
+  }, MISSION_TIMEOUT, 'mission receiving took too long');
 
-    // Return the mission and respond with an ACK message after each
-    // item until a mission is being received again and return the
-    // mission.
-    let complete = () => {
-        cleanupObj.finish(undefined, mission);
+  // Return the mission and respond with an ACK message after each
+  // item until a mission is being received again and return the
+  // mission.
+  let complete = () => {
+    cleanupObj.finish(undefined, mission);
 
-        let onItemAfter = () => _sendMissionAck(mav);
+    let onItemAfter = () => _sendMissionAck(mav);
 
-        // Send an ACK immediately regardless.
-        onItemAfter();
+    // Send an ACK immediately regardless.
+    onItemAfter();
 
-        mav.on('MISSION_ITEM', onItemAfter);
+    mav.on('MISSION_ITEM', onItemAfter);
 
-        // The next mission count message is the same as the count
-        // received in the next receiveMission call.
-        mav.once('MISSION_COUNT', () => {
-            mav.removeListener('MISSION_ITEM', onItemAfter);
-        });
-    };
+    // The next mission count message is the same as the count
+    // received in the next receiveMission call.
+    mav.once('MISSION_COUNT', () => {
+      mav.removeListener('MISSION_ITEM', onItemAfter);
+    });
+  };
 
-    // Request the list of mission items still remaining.
-    let requestRemaining = () => {
-        itemsRemaining.forEach(i => _sendMissionRequest(mav, i));
-    };
+  // Request the list of mission items still remaining.
+  let requestRemaining = () => {
+    itemsRemaining.forEach(i => _sendMissionRequest(mav, i));
+  };
 
-    // Send the mission request list message repetitively.
-    _sendMissionRequestList(mav);
-    reqListInt = setInterval(() => _sendMissionRequestList(mav), 1000);
+  // Send the mission request list message repetitively.
+  _sendMissionRequestList(mav);
+  reqListInt = setInterval(() => _sendMissionRequestList(mav), 1000);
 
-    // When a count message is received, create the mission object,
-    // the list of remaining fields, and finish if the count is zero,
-    // otherwise, start a task that requests the remaining items.
-    onCount = (fields) => {
-        let count = fields.count;
+  // When a count message is received, create the mission object, the
+  // list of remaining fields, and finish if the count is zero,
+  // otherwise, start a task that requests the remaining items.
+  onCount = (fields) => {
+    let count = fields.count;
 
-        mission.mission_items = Array(count).fill({});
-        itemsRemaining = _.range(count);
+    mission.mission_items = Array(count).fill({});
+    itemsRemaining = _.range(count);
 
-        if (count === 0) {
-            complete();
-        } else {
-            requestRemaining();
-            reqItemInt = setInterval(requestRemaining, 1000);
-        }
-    };
+    if (count === 0) {
+      complete();
+    } else {
+      requestRemaining();
+      reqItemInt = setInterval(requestRemaining, 1000);
+    }
+  };
 
-    // For each item, check if it's remaining, and if it is, add it
-    // to the mission item list and pop it off the remaining list.
-    onItem = (fields) => {
-        let id = fields.seq;
-        let remainingIndex = itemsRemaining.indexOf(id);
+  // For each item, check if it's remaining, and if it is, add it to
+  // the mission item list and pop it off the remaining list.
+  onItem = (fields) => {
+    let id = fields.seq;
+    let remainingIndex = itemsRemaining.indexOf(id);
 
-        if (remainingIndex !== -1) {
-            mission.mission_items[id] = _convertMavlinkMission(fields);
-            itemsRemaining.splice(remainingIndex, 1);
-        }
+    if (remainingIndex !== -1) {
+      mission.mission_items[id] = _convertMavlinkMission(fields);
+      itemsRemaining.splice(remainingIndex, 1);
+    }
 
-        if (itemsRemaining.length === 0) {
-            complete();
-        }
-    };
+    if (itemsRemaining.length === 0) {
+      complete();
+    }
+  };
 
-    mav.once('MISSION_COUNT', onCount);
-    mav.on('MISSION_ITEM', onItem);
+  mav.once('MISSION_COUNT', onCount);
+  mav.on('MISSION_ITEM', onItem);
 
-    return await cleanupObj.wait();
+  return await cleanupObj.wait();
 }
 
 /**
@@ -132,148 +132,148 @@ export async function receiveMission(mav) {
  * @returns {Promise}
  */
 export async function sendMission(mav, mission) {
-    // If this is an empty list, clear the mission instead.
-    if (!mission || mission.mission_items.length === 0) {
-        await _clearAll(mav);
-    } else {
-        await _sendMission(mav, mission);
-    }
+  // If this is an empty list, clear the mission instead.
+  if (!mission || mission.mission_items.length === 0) {
+    await _clearAll(mav);
+  } else {
+    await _sendMission(mav, mission);
+  }
 }
 
 async function _clearAll(mav) {
-    let clearInt;
-    let onAck;
+  let clearInt;
+  let onAck;
 
-    let cleanupObj = cleanupAfter(() => {
-        clearInterval(clearInt);
-        mav.removeListener('MISSION_ACK', onAck);
-    }, MISSION_TIMEOUT, 'mission clearing took too long');
+  let cleanupObj = cleanupAfter(() => {
+    clearInterval(clearInt);
+    mav.removeListener('MISSION_ACK', onAck);
+  }, MISSION_TIMEOUT, 'mission clearing took too long');
 
-    // Send a clear message repetitively.
-    _sendClearAll(mav);
-    clearInt = setInterval(() => _sendClearAll(mav), 1000);
+  // Send a clear message repetitively.
+  _sendClearAll(mav);
+  clearInt = setInterval(() => _sendClearAll(mav), 1000);
 
-    // When the ACK comes, finish this exchange.
-    onAck = _getAckHandler(cleanupObj);
+  // When the ACK comes, finish this exchange.
+  onAck = _getAckHandler(cleanupObj);
 
-    mav.once('MISSION_ACK', onAck);
+  mav.once('MISSION_ACK', onAck);
 
-    await cleanupObj.wait();
+  await cleanupObj.wait();
 }
 
 async function _sendMission(mav, mission) {
-    let items = mission.mission_items;
+  let items = mission.mission_items;
 
-    let countInt;
-    let onFirstRequest;
-    let onRequest;
-    let onAck;
+  let countInt;
+  let onFirstRequest;
+  let onRequest;
+  let onAck;
 
-    let cleanupObj = cleanupAfter(() => {
-        clearInterval(countInt);
-        mav.removeListener('MISSION_REQUEST', onFirstRequest);
-        mav.removeListener('MISSION_REQUEST', onRequest);
-        mav.removeListener('MISSION_ACK', onAck);
-    }, MISSION_TIMEOUT, 'mission sending took too long');
+  let cleanupObj = cleanupAfter(() => {
+    clearInterval(countInt);
+    mav.removeListener('MISSION_REQUEST', onFirstRequest);
+    mav.removeListener('MISSION_REQUEST', onRequest);
+    mav.removeListener('MISSION_ACK', onAck);
+  }, MISSION_TIMEOUT, 'mission sending took too long');
 
-    // Send a count once, and then keep doing it on an interval if a
-    // mission request hasn't come in yet.
-    _sendCount(mav, items.length)
-    countInt = setInterval(() => _sendCount(mav, items.length), 1000);
+  // Send a count once, and then keep doing it on an interval if a
+  // mission request hasn't come in yet.
+  _sendCount(mav, items.length);
+  countInt = setInterval(() => _sendCount(mav, items.length), 1000);
 
-    // After the first request, stop the count interval.
-    onFirstRequest = () => clearInterval(countInt);
+  // After the first request, stop the count interval.
+  onFirstRequest = () => clearInterval(countInt);
 
-    // On each request, send back the mission item that is requested.
-    onRequest = fields => _sendMissionItem(mav, items[fields.seq]);
+  // On each request, send back the mission item that is requested.
+  onRequest = fields => _sendMissionItem(mav, items[fields.seq]);
 
-    // When the ACK comes, finish this exchange.
-    onAck = _getAckHandler(cleanupObj);
+  // When the ACK comes, finish this exchange.
+  onAck = _getAckHandler(cleanupObj);
 
-    mav.once('MISSION_REQUEST', onFirstRequest);
-    mav.on('MISSION_REQUEST', onRequest);
-    mav.once('MISSION_ACK', onAck);
+  mav.once('MISSION_REQUEST', onFirstRequest);
+  mav.on('MISSION_REQUEST', onRequest);
+  mav.once('MISSION_ACK', onAck);
 
-    await cleanupObj.wait();
+  await cleanupObj.wait();
 }
 
 async function _sendMissionRequestList(mav) {
-    await mav.send('MISSION_REQUEST_LIST', {
-        target_system: 1,
-        target_component: 1,
-        mission_type: 0
-    });
+  await mav.send('MISSION_REQUEST_LIST', {
+    target_system: 1,
+    target_component: 1,
+    mission_type: 0
+  });
 }
 
 async function _sendMissionRequest(mav, id) {
-    await mav.send('MISSION_REQUEST', {
-        target_system: 1,
-        target_component: 1,
-        seq: id,
-        mission_type: 0
-    });
+  await mav.send('MISSION_REQUEST', {
+    target_system: 1,
+    target_component: 1,
+    seq: id,
+    mission_type: 0
+  });
 }
 
 async function _sendMissionAck(mav) {
-    await mav.send('MISSION_ACK', {
-        target_system: 1,
-        target_component: 1,
-        type: 0,
-        mission_type: 0
-    });
+  await mav.send('MISSION_ACK', {
+    target_system: 1,
+    target_component: 1,
+    type: 0,
+    mission_type: 0
+  });
 }
 
 async function _sendClearAll(mav) {
-    await mav.send('MISSION_CLEAR_ALL', {
-        target_system: 1,
-        target_component: 1,
-        mission_type: 255
-    });
+  await mav.send('MISSION_CLEAR_ALL', {
+    target_system: 1,
+    target_component: 1,
+    mission_type: 255
+  });
 }
 
 async function _sendCount(mav, count) {
-    await mav.send('MISSION_COUNT', {
-        target_system: 1,
-        target_component: 1,
-        count: count,
-        mission_type: 0
-    });
+  await mav.send('MISSION_COUNT', {
+    target_system: 1,
+    target_component: 1,
+    count: count,
+    mission_type: 0
+  });
 }
 
 async function _sendMissionItem(mav, item) {
-    await mav.send('MISSION_ITEM', _convertProtoMission(item));
+  await mav.send('MISSION_ITEM', _convertProtoMission(item));
 }
 
 // Return a handler that finishes with an error on a bad ACK message.
 function _getAckHandler(cleanupObj) {
-    return (fields) => {
-        if (fields.type === 0) {
-            cleanupObj.finish();
-        } else {
-            let err = Error('MISSION_ACK returned CMD ID ' + fields.type);
-            cleanupObj.finish(err);
-        }
+  return (fields) => {
+    if (fields.type === 0) {
+      cleanupObj.finish();
+    } else {
+      let err = Error('MISSION_ACK returned CMD ID ' + fields.type);
+      cleanupObj.finish(err);
     }
+  };
 }
 
 function _convertMavlinkMission(fields) {
-    let itemCopy = JSON.parse(JSON.stringify(fields));
+  let itemCopy = JSON.parse(JSON.stringify(fields));
 
-    itemCopy.param_1 = itemCopy.param1;
-    itemCopy.param_2 = itemCopy.param2;
-    itemCopy.param_3 = itemCopy.param3;
-    itemCopy.param_4 = itemCopy.param4;
+  itemCopy.param_1 = itemCopy.param1;
+  itemCopy.param_2 = itemCopy.param2;
+  itemCopy.param_3 = itemCopy.param3;
+  itemCopy.param_4 = itemCopy.param4;
 
-    return itemCopy;
+  return itemCopy;
 }
 
 function _convertProtoMission(mission) {
-    let itemCopy = JSON.parse(JSON.stringify(mission));
+  let itemCopy = JSON.parse(JSON.stringify(mission));
 
-    itemCopy.param1 = itemCopy.param_1;
-    itemCopy.param2 = itemCopy.param_2;
-    itemCopy.param3 = itemCopy.param_3;
-    itemCopy.param4 = itemCopy.param_4;
+  itemCopy.param1 = itemCopy.param_1;
+  itemCopy.param2 = itemCopy.param_2;
+  itemCopy.param3 = itemCopy.param_3;
+  itemCopy.param4 = itemCopy.param_4;
 
-    return itemCopy;
+  return itemCopy;
 }
--- services/telemetry/src/util.js
+++ services/telemetry/src/util.js
@@ -4,14 +4,14 @@
  * is `application/json`.
  */
 export function sendJsonOrProto(req, res, proto) {
-    const accept = req.get('accept');
+  const accept = req.get('accept');
 
-    if (accept === undefined || !accept.startsWith('application/json')) {
-        res.set('content-type', 'application/x-protobuf');
-        res.send(proto.constructor.encode(proto).finish());
-    } else {
-        res.send(proto.constructor.toObject(proto));
-    }
+  if (accept === undefined || !accept.startsWith('application/json')) {
+    res.set('content-type', 'application/x-protobuf');
+    res.send(proto.constructor.encode(proto).finish());
+  } else {
+    res.send(proto.constructor.toObject(proto));
+  }
 }
 
 /**
@@ -19,51 +19,51 @@ export function sendJsonOrProto(req, res, proto) {
  * https://stackoverflow.com/a/3417242
  */
 export function wrapIndex(i, i_max) {
-    return ((i % i_max) + i_max) % i_max;
+  return ((i % i_max) + i_max) % i_max;
 }
 
 /** Wait for an amount of time. */
 export async function wait(milliseconds) {
-    return await (new Promise(resolve => setTimeout(resolve, milliseconds)));
+  return await (new Promise(resolve => setTimeout(resolve, milliseconds)));
 }
 
 /** Return a object that can be used for cleanup in timeouts. */
 export function cleanupAfter(cleanup, limit, message) {
-    let timeoutMessage = message || 'operation timed out';
+  let timeoutMessage = message || 'operation timed out';
 
-    let earlyCall = false;
-    let earlyErr = null;
+  let earlyCall = false;
+  let earlyErr = null;
 
-    // Store if there was an error or not in the case that the finish
-    // function is called before it is overrided below.
-    let finish = (err) => {
-        earlyCall = true;
-        earlyErr = err;
+  // Store if there was an error or not in the case that the finish
+  // function is called before it is overrided below.
+  let finish = (err) => {
+    earlyCall = true;
+    earlyErr = err;
+  };
+
+  // Resolves after the cleanup is finished.
+  let done = new Promise((resolve, reject) => {
+    // Call the finish function if the timeout is reached.
+    let waitTimeout = setTimeout(() => {
+      finish(Error(timeoutMessage));
+    }, limit);
+
+    // Cancel the timeout, cleanup, and mark the operation as
+    // complete.
+    finish = (err, result) => {
+      clearTimeout(waitTimeout);
+      cleanup();
+
+      if (err) reject(err);
+      else resolve(result);
     };
 
-    // Resolves after the cleanup is finished.
-    let done = new Promise((resolve, reject) => {
-        // Call the finish function if the timeout is reached.
-        let waitTimeout = setTimeout(() => {
-            finish(Error(timeoutMessage));
-        }, limit);
+    if (earlyCall) finish(earlyErr);
+  });
 
-        // Cancel the timeout, cleanup, and mark the operation as
-        // complete.
-        finish = (err, result) => {
-            clearTimeout(waitTimeout);
-            cleanup();
+  // Async function to wait for the operation to be over and cleaned
+  // up.
+  let wait = async () => await done;
 
-            if (err) reject(err);
-            else resolve(result);
-        };
-
-        if (earlyCall) finish(earlyErr);
-    });
-
-    // Async function to wait for the operation to be over and
-    // cleaned up.
-    let wait = async () => await done;
-
-    return { finish, wait };
+  return { finish, wait };
 }
