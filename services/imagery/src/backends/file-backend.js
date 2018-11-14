ce14a15cc1353a4a85a5bb44a39d1a93660cd39e
Bradley Bridges <blbridges96@gmail.com>
2018-11-14 13:27:30 -0600

    Print file watching errors in the file backend

--- services/imagery/src/backends/file-backend.js
+++ services/imagery/src/backends/file-backend.js
@@ -1,6 +1,7 @@
 import chokidar from 'chokidar';
 import fs from 'fs-extra';
 
+import logger from '../../src/common/logger';
 import { imagery } from '../messages';
 
 import { convertPng, removeExif } from '../util';
@@ -31,37 +32,46 @@ export default class FileBackend {
 
     this._watcher = chokidar.watch(WATCH_FOLDER_NAME)
       .on('add', async (path) => {
-        // Check if it's a JPEG or PNG image.
-        const isJpeg = path.match(/\.jpe?g$/i);
-        const isPng = path.match(/\.png$/i);
-
-        // Don't process this if it isn't either of the
-        // above.
-        if (!(isJpeg || isPng)) return;
-
-        // On each new file, read and then delete it.
-        let data = await fs.readFile(path, { encoding: null });
-        await fs.unlink(path);
-
-        // If this is a PNG, we'll go ahead and convert it to a JPEG.
-        if (isPng) data = await convertPng(data);
-
-        // Taking off EXIF data to prevent image preview applications
-        // from rotating it.
-        data = await removeExif(data);
-
-        // The only metadata here is the timestamp.
-        const metadata = imagery.Image.create({
-          time: (new Date()).getTime() / 1000
-        });
-
-        // Add it to the image store.
-        await this._imageStore.addImage(data, metadata);
+        try {
+          await this._handleFile(path);
+        } catch (err) {
+          const message = err.name + ': ' + err.message;
+          logger.error('Encountered an error in file watch loop: ' + message);
+        }
       });
   }
 
   /** Stop watching files. */
   async stop() {
     this._watcher.close();
+  }
+
+  async _handleFile(path) {
+    // Check if it's a JPEG or PNG image.
+    const isJpeg = path.match(/\.jpe?g$/i);
+    const isPng = path.match(/\.png$/i);
+
+    // Don't process this if it isn't either of the
+    // above.
+    if (!(isJpeg || isPng)) return;
+
+    // On each new file, read and then delete it.
+    let data = await fs.readFile(path, { encoding: null });
+    await fs.unlink(path);
+
+    // If this is a PNG, we'll go ahead and convert it to a JPEG.
+    if (isPng) data = await convertPng(data);
+
+    // Taking off EXIF data to prevent image preview applications
+    // from rotating it.
+    data = await removeExif(data);
+
+    // The only metadata here is the timestamp.
+    const metadata = imagery.Image.create({
+      time: (new Date()).getTime() / 1000
+    });
+
+    // Add it to the image store.
+    await this._imageStore.addImage(data, metadata);
   }
 }
