f91645c87b95dfc44245b9cbdfed8b3efc808606
Bradley Bridges <blbridges96@gmail.com>
2019-02-20 14:34:16 -0600

    Fix tests to use only one redis instance
    Instead a new aioredis instance is made for each test, and the redis
    instance is flushed for each test.

--- services/image-rec-master/test/conftest.py
+++ services/image-rec-master/test/conftest.py
@@ -5,9 +5,12 @@ import docker
 import pytest
 
 
-@pytest.fixture
-async def redis():
-    """Starts up and stops a redis instance with a client."""
+@pytest.fixture(scope='module')
+def redis_container():
+    """Starts up and stops a Redis instance.
+
+    Intended to run just once for the duration of the tests.
+    """
     client = docker.DockerClient(base_url='unix://var/run/docker.sock')
     api_client = docker.APIClient(base_url='unix://var/run/docker.sock')
 
@@ -16,10 +19,19 @@ async def redis():
     inspect_dict = api_client.inspect_container(redis_container.name)
     ip = inspect_dict['NetworkSettings']['IPAddress']
 
-    redis = await aioredis.create_redis_pool((ip, 6379), minsize=5, maxsize=10)
+    yield (ip, 6379)
+
+    redis_container.kill()
+
+
+@pytest.fixture
+async def redis(redis_container):
+    """Creates an aioredis instance for the Redis container."""
+    redis = await aioredis.create_redis_pool(redis_container, minsize=5,
+                                             maxsize=10)
+    await redis.flushdb()
 
     yield redis
 
-    redis_container.kill()
     redis.close()
     await redis.wait_closed()
