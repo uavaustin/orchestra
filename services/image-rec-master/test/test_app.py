05dfbfdbff11f25db03420f075c4bc4ab06cfc75
Bradley Bridges <blbridges96@gmail.com>
2019-02-20 18:26:35 -0600

    Add a test for getting a pipeline target by id

--- services/image-rec-master/test/test_app.py
+++ services/image-rec-master/test/test_app.py
@@ -2,6 +2,7 @@ from aiohttp import web
 import pytest
 
 from messages.image_rec_pb2 import PipelineState, Target
+from messages.interop_pb2 import Odlc
 
 from service.app import routes
 
@@ -34,6 +35,28 @@ async def test_get_pipeline(app_client, redis):
     assert msg.unprocessed_auto == [6]
     assert msg.processed_auto == []
     assert msg.all_targets == []
+
+
+async def test_get_pipeline_target_by_id(app_client, redis):
+    # Add a target to fetch.
+    odlc = Odlc()
+    odlc.id = 6
+    target = ('id', 4, 'image_id', 5, 'odlc', odlc.SerializeToString(),
+              'submitted', 1, 'errored', 0, 'removed', 0)
+
+    await redis.sadd('all-targets', 4)
+    await redis.hmset('target:4', *target)
+
+    resp = await app_client.get('/api/pipeline/targets/4')
+    assert resp.status == 200
+
+    msg = Target.FromString(await resp.read())
+    assert msg.id == 4
+    assert msg.image_id == 5
+    assert msg.odlc.id == 6
+    assert msg.submitted is True
+    assert msg.errored is False
+    assert msg.removed is False
 
 
 async def test_reset_pipeline(app_client, redis):
