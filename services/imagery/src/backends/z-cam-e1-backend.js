13c01df513b0e0fb44f8f945c5f48c3aa3499e26
Rahul Butani <rr.butani@gmail.com>
2019-06-11 03:14:14 -0500

    z-cam-e1: clean up the config items

--- services/imagery/src/backends/z-cam-e1-backend.js
+++ services/imagery/src/backends/z-cam-e1-backend.js
@@ -1,6 +1,47 @@
 import request from 'superagent';
 
+import logger from '../common/logger';
+
 import CameraBaseBackend from './camera-base-backend';
+
+const configuration = [
+  ['drive_mode', 'single'],           // Single shot drive mode
+  ['photosize', '16M'],               // 16M
+  ['wb', 'Auto'],                     // Auto WB (White Balance)
+  ['iso', 'Auto'],                    // Auto ISO
+  ['sharpness', 'Normal'],            // Normal Sharpness
+  ['contrast', '64'],                 // 64/256 for Contrast
+  ['saturation', '64'],               // 64/256 for Saturation
+  ['brightness', '0'],                // Brightness: 0 [-256, 256]
+  ['meter_mode', 'Average'],          // Average for Meter Mode
+  ['ev', '0'],                        // EV: 0 (to start) [-96, 96]
+  ['lcd', 'Off'],                     // LCD off (save some power)
+  ['rotation', 'Normal'],             // We're not upside down!
+  ['focus', 'AF'],                    // Enable Autofocus
+  ['af_mode', 'Normal'],              // Normal Autofocus mode
+  ['photo_q', 'S.Fine'],              // Photo Quality
+  ['led', 'On'],                      // LEDs on!
+  ['max_exp', 'Auto'],                // Auto Exposure Time
+  ['shutter_angle', 'Auto'],          // Shouldn't matter
+  // Skipping "mwb"; this should have an invalid value when queried.
+  ['shutter_spd', 'Auto'],            // Auto Shutter Speed
+  ['caf_range', 'Auto'],              // Auto Continuous AF Range
+  ['caf_sens', 'Middle'],             // Middle CAF Sensitivity
+  ['lut', 'sRGB'],                    // sRGB lookup
+  ['dewarp', 'On'],                   // Distortion Correction: On
+  ['vignette', 'Off'],                // Vignette Correction: Off
+  ['noise_reduction', 'NormalNoise'], // Default Noise Reduction
+  ['lcd_backlight', '25'],            // Dim the screen
+  ['oled', 'Off'],                    // Turn off the OLED Screen
+  ['shoot_mode', 'Program AE'],       // Auto Mode, roughly.
+  ['liveview_audio', 'On'],           // Listen to the wind howling
+  ['max_iso', '6400'],                // Max ISO to absolute max
+  ['auto_off', '0'],                  // Disable auto-off
+  ['auto_off_lcd', '30'],             // 30s LCD timeout
+  ['caf', '1'],                       // Continuous AF: On
+  ['grid_display', '0'],              // No grids!
+  ['level_correction', '0'],          // Level Correction: Off
+];
 
 export default class ZCamE1Backend extends CameraBaseBackend {
   /**
@@ -19,62 +60,37 @@ export default class ZCamE1Backend extends CameraBaseBackend {
   async acquire() {
     await this._makeReq('/ctrl/session');
 
-    // Set the date
+    // Set the date. (no left-pad)
     const datetime = new Date();
     const time  = datetime.toTimeString().split(' ')[0];
-    const year  = `${datetime.getFullYear()}`.padStart(4, '0'); // no left-pad
+    const year  = `${datetime.getFullYear()}`.padStart(4, '0');
     const month = `${datetime.getMonth()}`.padStart(2, '0');
     const day   = `${datetime.getDate()}`.padStart(2, '0');
     const date  = `${year}-${month}-${day}`;
-    await this._setParam(`/datetime?date=${date}&time=${datetime}`);
+    await this._setParam(`/datetime?date=${date}&time=${time}`);
 
-    await this._makeReq('/ctrl/mode?action=to_cap'); // Put us in capture mode
-    await this._setParam('drive_mode', 'single');  // Single shot drive mode
-    await this._setParam('photosize', '16M');      // 16M
-    await this._setParam('wb', 'Auto');            // Auto WB (White Balance)
-    await this._setParam('iso', 'Auto');           // Auto ISO
-    await this._setParam('sharpness', 'Normal');   // Normal Sharpness
-    await this._setParam('contrast', '64');        // 64/256 for Contrast
-    await this._setParam('saturation', '64');      // 64/256 for Saturation
-    await this._setParam('brightness', '0');       // Brightness: 0 [-256, 256]
-    await this._setParam('meter_mode', 'Average'); // Average for Meter Mode
-    await this._setParam('ev', '0');               // EV: 0 (to start) [-96, 96]
-    await this._setParam('lcd', 'Off')             // LCD off (save some power)
-    await this._setParam('rotation', 'Normal')     // We're not upside down!
-    await this._setParam('focus', 'AF')            // Enable Autofocus
-    await this._setParam('af_mode', 'Normal')      // Normal Autofocus mode
-    await this._setParam('photo_q', 'S.Fine');     // Photo Quality
-    await this._setParam('led', 'On');             // LEDs on!
-    await this._setParam('max_exp', 'Auto');       // Auto Exposure Time
-    await this._setParam('shutter_angle', 'Auto'); // Shouldn't matter
-    // Skipping "mwb"; this should have an invalid negative value when queried.
-    await this._setParam('shutter_spd', 'Auto');   // Auto Shutter Speed
-    await this._setParam('caf_range', 'Auto');     // Auto Continuous AF Range
-    await this._setParam('caf_sens', 'Middle');    // Middle CAF Sensitivity
-    await this._setParam('lut', 'sRGB');           // sRGB lookup
-    await this._setParam('dewarp', 'On');          // Distortion Correction: On
-    await this._setParam('vignette', 'Off');       // Vignette Correction: Off
-    await this._setParam('noise_reduction', 'NormalNoise');
-    await this._setParam('lcd_backlight', '25');   // Dim the screen
-    await this._setParam('oled', 'Off');           // Turn off the OLED Screen
-    await this._setParam('shoot_mode', 'Program AE');
-    await this._setParam('liveview_audio', 'On');
-    await this._setParam('max_iso', '6400');       // Max ISO to absolute max
-    await this._setParam('auto_off', '0');         // Disable auto-off
-    await this._setParam('auto_off_lcd', '30');    // 30s LCD timeout
-    await this._setParam('caf', '1');              // Continuous AF: On
-    await this._setParam('grid_display', '0');     // No grids!
-    await this._setParam('level_correction', '0'); // Level Correction: Off
+    // Put us in capture mode:.
+    await this._makeReq('/ctrl/mode?action=to_cap');
 
-    // Query:
-    //  - the battery level
-    //  - the temperature
-    //  - number of remaining shots
-    //  - aperture (iris)
-    //  - last file name
-    //  - camera rot
-    //  - file number
-    // and say something.
+    // Load the configuration.
+    configuration.forEach(async pair => {
+      const [key, val] = pair;
+      await this._makeReq(key, val);
+    });
+
+    const temperature = await this._makeReq('/ctrl/temperature');
+    logger.debug(`Temperature: ${temperature['msg']}`);
+
+    const remaining_shots = await this._makeReq('/ctrl/still?action=remain');
+    logger.debug(`Remaining Shots: ${remaining_shots['msg']}`);
+
+    logger.debug(`Battery Level: ${await this._getParam('battery')}`);
+    logger.debug(`Aperture: f/${await this._getParam('iris')}`);
+    logger.debug(`Last File Name: ${await this._getParam('last_file_name')}`);
+    logger.debug(`Camera Rotation: ${await this._getParam('camera_rot')}`);
+    logger.debug(`File Number: ${await this._getParam('file_number')}`);
+
+    logger.debug('📷 ready.');
   }
 
   /** Focus, capture an image, download it, and remove it. */
@@ -116,6 +132,11 @@ export default class ZCamE1Backend extends CameraBaseBackend {
   }
 
   async _setParam(param, value) {
-    await this._makeReq(`/ctrl/set?${param}=${value}`)
+    await this._makeReq(`/ctrl/set?${param}=${value}`);
+  }
+
+  async _getParam(param) {
+    const resp = await this._makeReq(`/ctrl/get?k=${param}`);
+    return resp['value'];
   }
 }
