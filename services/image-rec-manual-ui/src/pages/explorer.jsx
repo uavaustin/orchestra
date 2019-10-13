074960f3f74e41ea6b3ffa6d976fcc4184b9f211
Andy Ni <nakukibo@gmail.com>
2019-10-13 14:58:08 -0500

    Explorer: basic skeleton

--- services/image-rec-manual-ui/src/pages/explorer.jsx
+++ services/image-rec-manual-ui/src/pages/explorer.jsx
@@ -1,11 +1,110 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, List, Image, Grid, Card, Icon, Collapse } from 'semantic-ui-react';
+import './css/explorer.css';
 
 const Explorer = () => (
-  <Container id='page'>
+  <Container id='page' fluid>
     <Header as='h1'>Explorer</Header>
-    <em>TODO</em>
+    <Grid columns={3} id="mainBody">
+      <Grid.Row id="mainRow">
+        <Grid.Column width="3">
+          <List id="fileList">
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+            <List.Item>example</List.Item>
+          </List>
+        </Grid.Column>
+        <Grid.Column  width="10">
+          <Image src='https://images.unsplash.com/photo-1489438497675-d1a8d6e0632e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=973&q=80' />
+        </Grid.Column>
+        <Grid.Column width="3">
+          <List>
+            <List.Item icon='users' content='Some' />
+            <List.Item icon='marker' content='Info' />
+            <List.Item icon='mail' content='Here'/>
+          </List>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+​
   </Container>
 );
-
 export default Explorer;
+Collapse
