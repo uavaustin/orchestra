9b913681bbc6f2d0108c2ce46eb24d1efd79b113
Rahul Butani <rr.butani@gmail.com>
2019-10-13 16:47:35 -0500

    z-cam-e1: camera setup tweaks from before the competition, part 1

--- services/imagery/src/backends/z-cam-e1-backend.js
+++ services/imagery/src/backends/z-cam-e1-backend.js
@@ -3,19 +3,23 @@ import request from 'superagent';
 import logger from '../common/logger';
 
 import CameraBaseBackend from './camera-base-backend';
+import { wait } from '../util';
 
 const configuration = [
   ['drive_mode', 'single'],           // Single shot drive mode
   ['photosize', '16M'],               // 16M
   ['wb', 'Auto'],                     // Auto WB (White Balance)
   ['iso', 'Auto'],                    // Auto ISO
-  ['sharpness', 'Normal'],            // Normal Sharpness
+  //['sharpness', ' Normal '],          // ' Normal ' Sharpness
   ['contrast', '64'],                 // 64/256 for Contrast
   ['saturation', '64'],               // 64/256 for Saturation
   ['brightness', '0'],                // Brightness: 0 [-256, 256]
   ['meter_mode', 'Average'],          // Average for Meter Mode
   ['ev', '0'],                        // EV: 0 (to start) [-96, 96]
-  ['lcd', 'Off'],                     // LCD off (save some power)
+  ['lcd_backlight', '20'],            // Dim the screen
+  ['lcd', '0'],                       // Guarantee a timeout
+  ['auto_off', '0'],                  // Disable auto-off (will brick!)
+  ['auto_off_lcd', '30'],             // 30s LCD timeout
   ['rotation', 'Normal'],             // We're not upside down!
   ['focus', 'AF'],                    // Enable Autofocus
   ['af_mode', 'Normal'],              // Normal Autofocus mode
@@ -24,20 +28,17 @@ const configuration = [
   ['max_exp', 'Auto'],                // Auto Exposure Time
   ['shutter_angle', 'Auto'],          // Shouldn't matter
   // Skipping "mwb"; this should have an invalid value when queried.
-  ['shutter_spd', 'Auto'],            // Auto Shutter Speed
-  ['caf_range', 'Auto'],              // Auto Continuous AF Range
+  // ['shutter_spd', 'Auto'],            // Auto Shutter Speed
+  ['caf_range', 'Normal'],            // Auto Continuous AF Range
   ['caf_sens', 'Middle'],             // Middle CAF Sensitivity
   ['lut', 'sRGB'],                    // sRGB lookup
   ['dewarp', 'On'],                   // Distortion Correction: On
   ['vignette', 'Off'],                // Vignette Correction: Off
   ['noise_reduction', 'NormalNoise'], // Default Noise Reduction
-  ['lcd_backlight', '25'],            // Dim the screen
-  ['oled', 'Off'],                    // Turn off the OLED Screen
+  ['oled', 'On'],                     // Turn off the OLED Screen
   ['shoot_mode', 'Program AE'],       // Auto Mode, roughly.
   ['liveview_audio', 'On'],           // Listen to the wind howling
   ['max_iso', '6400'],                // Max ISO to absolute max
-  ['auto_off', '0'],                  // Disable auto-off
-  ['auto_off_lcd', '30'],             // 30s LCD timeout
   ['caf', '1'],                       // Continuous AF: On
   ['grid_display', '0'],              // No grids!
   ['level_correction', '0'],          // Level Correction: Off
@@ -75,11 +76,12 @@ export default class ZCamE1Backend extends CameraBaseBackend {
     const month = `${datetime.getMonth()}`.padStart(2, '0');
     const day   = `${datetime.getDate()}`.padStart(2, '0');
     const date  = `${year}-${month}-${day}`;
-    await this._setParam(`/datetime?date=${date}&time=${time}`);
+    await this._makeReq(`/datetime?date=${date}&time=${time}`);
 
     // Load the configuration.
     for (const [key, val] of configuration) {
       await this._setParam(key, val);
+      await wait(100);
     }
 
     const temperature = await this._makeReq('/ctrl/temperature');
@@ -95,12 +97,18 @@ export default class ZCamE1Backend extends CameraBaseBackend {
     logger.debug(`File Number: ${await this._getParam('file_number')}`);
 
     logger.debug('📷 ready.');
+
+    return { count: 0 };
   }
 
   /** Focus, capture an image, download it, and remove it. */
-  async capture() {
+  async capture(state) {
     // No explicit focus position; figure out the area automatically.
-    await this._makeReq('/ctrl/af');
+    if (state.count++ % 5 === 0) {
+      await this._makeReq('/ctrl/af');
+      logger.crit(`FOCUSING`);
+      await wait(500);
+    }
 
     const filename = (await this._makeReq('/ctrl/still?action=single')).msg;
     const photo = await this._makeReq(filename, true);
