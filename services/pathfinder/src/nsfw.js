fb55c30d9be1dd39eb2295ddcf158de5d631da24
chaseguy15 <chase.aerospace@utexas.edu>
2020-02-06 20:24:34 -0600

    added nsfw structure

---
+++ services/pathfinder/src/nsfw.js
@@ -0,0 +1,55 @@
+/*
+TO DO:
+- Import Vehicle Avoidance Process
+- Import Pathfinder Process
+
+- Run VA constantly
+- Run PF with conditionally
+-- VA calls
+-- New WPs
+
+
+RULES:
+- VA and PF cannot run at the same time
+- PF called any time WP changes
+- VA runs when PF is not runing
+- If VA goes off, calls PF to run again
+*/
+
+
+
+
+// imports
+
+
+
+// NOT SUITED FOR WIND
+
+export default class NSFW {
+  /**
+  * @param {Object} options
+  * @param {string} options.host
+  * @param {number} options.port
+  *
+  */
+  // separate for params for PF and VA?
+
+  constructor(options) {
+    // need to determine what is needed for construction
+
+
+    // overview?
+
+
+
+  }
+// start PF process (WP)
+// stop PF process
+
+// start VA process
+// stop VA process
+
+
+// get adjust path
+
+}
