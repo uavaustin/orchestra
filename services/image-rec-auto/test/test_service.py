78352d918e02dcf5b5e49d93f90ac395f5b1dbea
Kevin Li <likevinsen@gmail.com>
2021-01-07 02:24:45 -0600

    refactoring some tests + test for target queue

--- services/image-rec-auto/test/test_service.py
+++ services/image-rec-auto/test/test_service.py
@@ -12,11 +12,57 @@ from PIL import Image
 from messages import imagery_pb2
 from messages import image_rec_pb2
 import service
+
 from . import test_util
 
 
+class TestBase(unittest.TestCase):
+    imagery_host = "imagery"
+    imagery_port = 1234
+    master_host = "image-rec-master"
+    master_port = 1234
+    image_id = 0
+    url = f"http://{imagery_host}:{imagery_port}/api/image/{image_id}"
+
+    auto_service = service.Service(
+        imagery_host=imagery_host,
+        imagery_port=imagery_port,
+        master_host=master_host,
+        master_port=master_port,
+        fetch_interval=1,
+    )
+    image_id = 0
+    image_msg = imagery_pb2.Image()
+    image_msg.id = image_id
+    byteIO = io.BytesIO()
+    field_image = Image.open(test_util.TARGET_FIXTURE)
+    field_image.save(byteIO, format="JPEG")
+    image_msg.image = byteIO.getvalue()
+
+    def _mock_response(
+        self,
+        status=200,
+        content="CONTENT",
+        json_data=None,
+        raise_for_status=None,
+        ok=True
+    ) -> mock.Mock:
+        mock_resp = mock.Mock()
+        # mock raise_for_status call w/optional error
+        mock_resp.raise_for_status = mock.Mock()
+        if raise_for_status:
+            mock_resp.raise_for_status.side_effect = raise_for_status
+        # set status code and content
+        mock_resp.status_code = status
+        mock_resp.content = content
+        # add json data if provided
+        if json_data:
+            mock_resp.json = mock.Mock(return_value=json_data)
+        return mock_resp
+
+
 def test_target_rec():
-    def mock_queue(image_id, _image_proto, _image, targets):
+    def _mock_queue(image_id, _image_proto, _image, targets):
         assert image_id == 0
         assert len(targets) == 1
 
@@ -51,7 +97,7 @@ def test_target_rec():
 
         task_2.return_value = image_msg
         task_3.return_value = 1
-        task_3.side_effect = mock_queue
+        task_3.side_effect = _mock_queue
         task_4.return_value = 1
 
         auto_service = service.Service(
@@ -69,44 +115,8 @@ def test_target_rec():
         assert task_4.called
 
 
-class TestGetImage(unittest.TestCase):
-
-    imagery_host = "imagery"
-    imagery_port = 1234
-    master_host = "image-rec-master"
-    master_port = 1234
-    image_id = 0
-    url = f"http://{imagery_host}:{imagery_port}/api/image/{image_id}"
-
-    auto_service = service.Service(
-        imagery_host=imagery_host,
-        imagery_port=imagery_port,
-        master_host=master_host,
-        master_port=master_port,
-        fetch_interval=1,
-    )
-
-    image_msg = imagery_pb2.Image()
-    byteIO = io.BytesIO()
-    Image.open(test_util.TARGET_FIXTURE).save(byteIO, format="JPEG")
-    image_msg.image = byteIO.getvalue()
-    image_msg = image_msg.SerializeToString()
-
-    def _mock_response(
-        self, status=200, content="CONTENT", json_data=None, raise_for_status=None
-    ) -> mock.Mock:
-        mock_resp = mock.Mock()
-        # mock raise_for_status call w/optional error
-        mock_resp.raise_for_status = mock.Mock()
-        if raise_for_status:
-            mock_resp.raise_for_status.side_effect = raise_for_status
-        # set status code and content
-        mock_resp.status_code = status
-        mock_resp.content = content
-        # add json data if provided
-        if json_data:
-            mock_resp.json = mock.Mock(return_value=json_data)
-        return mock_resp
+class TestGetImage(TestBase):
+    image_msg = TestBase.image_msg.SerializeToString()
 
     @unittest.mock.patch("service.requests.get")
     def test_get_next_id(self, mock_get):
@@ -130,45 +140,9 @@ class TestGetImage(unittest.TestCase):
         self.assertEqual(mock_get.call_args.args[0], self.url)
 
 
-class TestGetNextId(unittest.TestCase):
+class TestGetNextId(TestBase):
 
-    imagery_host = "imagery"
-    imagery_port = 1234
-    master_host = "image-rec-master"
-    master_port = 1234
-    image_id = 0
-    url = f"http://{imagery_host}:{imagery_port}/api/image/{image_id}"
-
-    auto_service = service.Service(
-        imagery_host=imagery_host,
-        imagery_port=imagery_port,
-        master_host=master_host,
-        master_port=master_port,
-        fetch_interval=1,
-    )
-    image_id = 0
-    image_msg = image_rec_pb2.PipelineImage()
-    image_msg.id = image_id
-    image_msg = image_msg.SerializeToString()
-
-    def _mock_response(
-        self,
-        status=200,
-        content="CONTENT",
-        raise_for_status=None,
-        ok=True
-    ) -> mock.Mock:
-        mock_resp = mock.Mock()
-        # mock raise_for_status call w/optional error
-        mock_resp.raise_for_status = mock.Mock()
-        if raise_for_status:
-            mock_resp.raise_for_status.side_effect = raise_for_status
-        # set status code and content
-        mock_resp.status_code = status
-        mock_resp.content = content
-        mock_resp.ok = ok
-
-        return mock_resp
+    image_msg = TestBase.image_msg.SerializeToString()
 
     @unittest.mock.patch("service.requests.post")
     def test_get_next_id(self, mock_post):
@@ -181,33 +155,31 @@ class TestGetNextId(unittest.TestCase):
         self.assertEqual(retval, self.image_id)
 
 
-class TestRunIter(unittest.TestCase):
-    imagery_host = "imagery"
-    imagery_port = 1234
-    master_host = "image-rec-master"
-    master_port = 1234
-    image_id = 0
-    url = f"http://{imagery_host}:{imagery_port}/api/image/{image_id}"
+class TestRunIter(TestBase):
 
-    auto_service = service.Service(
-        imagery_host=imagery_host,
-        imagery_port=imagery_port,
-        master_host=master_host,
-        master_port=master_port,
-        fetch_interval=1,
-    )
-    image_id = 0
-    image_msg = imagery_pb2.Image()
-    image_msg.id = image_id
-    byteIO = io.BytesIO()
-    field_image = Image.open(test_util.TARGET_FIXTURE)
-    field_image.save(byteIO, format="JPEG")
-    image_msg.image = byteIO.getvalue()
+    @mock.patch.object(service.Service, "_get_next_id")
+    @mock.patch.object(service.Service, "_get_image")
+    def test_run_iter(
+        self,
+        mock_get_next_id,
+        mock_get_image,
+    ):
+        mock_get_next_id.return_value = self.image_id
+        mock_get_image.return_value = self.image_msg
+
+        self.auto_service.run_iter()
+
+        self.assertTrue(mock_get_next_id.called)
+        self.assertTrue(mock_get_image.called)
+
+
+class TestTargetQueue(TestBase):
 
     def _mock_response(
         self,
         status=200,
         content="CONTENT",
+        json_data=None,
         raise_for_status=None,
         ok=True
     ) -> mock.Mock:
@@ -219,22 +191,39 @@ class TestRunIter(unittest.TestCase):
         # set status code and content
         mock_resp.status_code = status
         mock_resp.content = content
-        mock_resp.ok = ok
-
+        # add json data if provided
+        if json_data:
+            mock_resp.json = mock.Mock(return_value=json_data)
         return mock_resp
 
-    @mock.patch.object(service.Service, "_get_next_id")
-    @mock.patch.object(service.Service, "_get_image")
-    def test_run_iter(
-        self,
-        mock_get_next_id,
-        mock_get_image,
-    ):
+    @mock.patch("service.requests.post")
+    def test_successful_queue(self, mock_post):
 
-        mock_get_next_id.return_value = self.image_id
-        mock_get_image.return_value = self.image_msg
+        mock_target = types.Target(
+            x=215,
+            y=265,
+            width=68,
+            height=70,
+            shape=types.Shape.TRAPEZOID,
+            alphanumeric="K",
+            image=TestBase.field_image
+        )
+        mock_resp = self._mock_response(status=201)
+        mock_post.return_value = mock_resp
 
-        self.auto_service.run_iter()
+        target_proto = image_rec_pb2.PipelineTarget()
+        target_proto.odlc.CopyFrom(
+            service.util.get_odlc(self.field_image, None, mock_target)
+        )
+        target_proto.image_id = 0
 
-        self.assertTrue(mock_get_next_id.called)
-        self.assertTrue(mock_get_image.called)
+        retval = self.auto_service._queue_targets(
+            0, imagery_pb2.Image(), self.field_image, [mock_target]
+        )
+
+        self.assertTrue(mock_post.called)
+        self.assertTrue(retval)
+        # data is in the second positional argument
+        self.assertEqual(
+            mock_post.call_args_list[0][1].get('data'), target_proto.SerializeToString()
+        )
