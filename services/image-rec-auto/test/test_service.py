0f8d4138c464bdeb116a6cda93c6bf5539c87883
Kevin Li <likevinsen@gmail.com>
2021-01-07 02:28:50 -0600

    remove redudant override

--- services/image-rec-auto/test/test_service.py
+++ services/image-rec-auto/test/test_service.py
@@ -12,7 +12,6 @@ from PIL import Image
 from messages import imagery_pb2
 from messages import image_rec_pb2
 import service
-
 from . import test_util
 
 
@@ -174,27 +173,6 @@ class TestRunIter(TestBase):
 
 
 class TestTargetQueue(TestBase):
-
-    def _mock_response(
-        self,
-        status=200,
-        content="CONTENT",
-        json_data=None,
-        raise_for_status=None,
-        ok=True
-    ) -> mock.Mock:
-        mock_resp = mock.Mock()
-        # mock raise_for_status call w/optional error
-        mock_resp.raise_for_status = mock.Mock()
-        if raise_for_status:
-            mock_resp.raise_for_status.side_effect = raise_for_status
-        # set status code and content
-        mock_resp.status_code = status
-        mock_resp.content = content
-        # add json data if provided
-        if json_data:
-            mock_resp.json = mock.Mock(return_value=json_data)
-        return mock_resp
 
     @mock.patch("service.requests.post")
     def test_successful_queue(self, mock_post):
