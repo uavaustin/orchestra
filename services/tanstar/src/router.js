acbe13e85b122bb6d3d610995d8dcd012d6cfbcd
chaseguy15 <chase.aerospace@utexas.edu>
2021-02-25 20:17:21 -0600

    removed problem routes, got basic test up

--- services/tanstar/src/router.js
+++ services/tanstar/src/router.js
@@ -2,7 +2,7 @@ import koaProtobuf from 'koa-protobuf';
 import Router from 'koa-router';
 
 import logger from './common/logger';
-//import { pathfinder } from './messages';
+// import { pathfinder } from './messages';
 
 const router = new Router();
 
@@ -19,18 +19,21 @@ let timeout = async (ctx, next) => {
   }
 };
 
+//let pf = koaProtobuf.protobufParser(pathfinder);
+
 // Middleware to parse a request for the raw path.
-let raw = koaProtobuf.protobufParser(X)
+//let request = koaProtobuf.protobufParser(pathfinder.Request);
 
 // Middleware to parse a request for the adjusted path.
-let adjusted = koaProtobuf.protobufParser(X)
-
+//let adjusted = koaProtobuf.protobufParser(pathfinder.AdjustedPath);
 
 router.get('/api/alive', (ctx) => {
   ctx.body = 'Yep, I am alive.\n';
 });
 
-/* Pushes in request data to path-adjust */
+/*
+
+
 // !! Will deprecate with more robust proto messages later
 router.put('/api/request', request, timeout, async (ctx) => {
   await ctx.PathAdjust.set(ctx.request.proto);
@@ -38,58 +41,18 @@ router.put('/api/request', request, timeout, async (ctx) => {
 });
 
 
-/* Returns response proto from path-adjust, pathfinder */
+
 // !! Will deprecate with more robust proto messages later
 router.get('/api/response', async (ctx) => {
-  const adjusted = await ctx.PathAdjust.getAdjusted();
-
-  //ctx.proto = pathfinder.Response.create({
-  //  success: True,
-  //  adjusted
-  //});
-});
-
-// This was my first attempt at fixing lines 35-50, but I know it's very wrong
-// so I commented it out.
-// Need to find correct way to unwrap protobufs, find right way to set plane
-// field, find right way to set rawPath field
-/*
-router.put('/api/request', Request, timeout, (ctx) => {
-  const new_request = ctx.Pathfinder.create({
-    flyzone: ctx.request.flyzones,
-    obstacles: ctx.request.obstacles,
-    plane: ctx.request.overview,
-    rawPath: ct.request.mission
-  })
-  await ctx.PathAdjust.set(new_request.flyzone,new_request.obstacle,new_request.plane,new_request.rawPath);
-
   const adjusted = await ctx.PathAdjust.getAdjusted();
 
   ctx.proto = pathfinder.Response.create({
     success: True,
     adjusted
-  })
+  });
 });
-*/
-
-/* Pushes in flyzones data to path-adjust */
-///router.get('/api/flyzones', (ctx) => {
-//});
 
 
-/* Pushes in obstacles data to path-adjust */
-///router.get('/api/obstacles', (ctx) => {
-//});
-
-/* Pushes in plane location data to path-adjust */
-///router.get('/api/plane', (ctx) => {
-//});
-
-/* Pushes in raw mission to path-adjust */
-///router.get('/api/obstacles', (ctx) => {
-//});
-
-/* Returns adjusted mission data proto */
 router.get('/api/adjusted-mission', async (ctx) => {
   const adjusted = await ctx.PathAdjust.getAdjusted();
 
@@ -99,5 +62,7 @@ router.get('/api/adjusted-mission', async (ctx) => {
   //  adjusted
   //});
 });
+
+*/
 
 export default router;
--- services/tanstar/tests/service.test.js
+++ services/tanstar/tests/service.test.js
@@ -1,37 +1,28 @@
-import nock from 'nock';
-
-import logger from '../src/common/logger';
-import { interop } from '../src/messages';
-
+import addProtobuf from 'superagent-protobuf';
 import Service from '../src/service';
+import request from 'supertest';
 
-// Creating service
-const service = new Service({
-    port: 8765,
+let service;
+
+// Bring the service up before other tests start.
+test('start a new service instance', async () => {
+  service = new Service({
+    port: 5000,
     serviceTimeout: 7000,
-    interopProxyHost: 'interop-proxy',
-    interopProxyPort: '8989'
-});
+    InteropProxyHost: 'interop-proxy',
+    InteropProxyPort: 8989
+  });
 
-beforeAll(async () => {
-    await service.start();
-});
+  await service.start();
+}, 4000);
 
-test('successful retrieval of obstacle data', async () => {
-    const exp = {
-      time: 1, stationary: [{ pos: {lat: 2, lon: 3}, height: 4, radius: 5}]
-    };
-    const obstacle = interop.Obstacles.encode(exp).finish();
+test('service is alive', async () => {
+  let res = await request('http://localhost:5000')
+    .get('/api/alive');
 
-    const interopProxyApi = nock('http://interop-proxy:8989')
-      .defaultReplyHeaders({ 'content-type': 'application/x-protobuf' })
-      .get('/api/obstacles')
-      .reply(200, obstacle);
-
-    const res = await service._retrieveObstacles();
-    
-    interopProxyApi.done(); // Check if API hit correctly
-    expect(res).toMatchObject(exp); // Check if response is correct
+  expect(res.status).toEqual(200);
+  expect(res.type).toEqual('text/plain');
+  expect(res.text).toBeTruthy();
 });
 
 afterAll(async () => {
